6464d6d231f4d2b59d1f9c5bb9d1c704c7f63ca0
Aniki Gasai <28224659+AnikiGasai@users.noreply.github.com>
2020-11-23 20:23:21 +0300

    Update CRect.cpp
    bounds fix

--- src/0.3.7-R1/CRect.cpp
+++ src/0.3.7-R1/CRect.cpp
@@ -10,13 +10,14 @@
 #include "CRect.h"
 
 SAMP::CRect::CRect() {
-	left, top = 0, 0;
-	right, bottom = 0, 0;
+	left = top = right = bottom = 0;
 }
 
 SAMP::CRect::CRect(long _left, long _top, long _right, long _bottom) {
-	left, top = _left, _top;
-	right, bottom = _right, _bottom;
+	left = _left;
+	top = _top;
+	right = _right;
+	bottom = _bottom;
 }
 
 long SAMP::CRect::GetWidth() {
@@ -30,8 +31,10 @@ long SAMP::CRect::GetHeight() {
 void SAMP::CRect::Move(long x, long y) {
 	long w = GetWidth();
 	long h = GetHeight(); 
-	left, top = x, y;
-	right, bottom = x + w, y + h;
+	left = x;
+	top = y;
+	right = x + w;
+	bottom =  y + h;
 }
 
 void SAMP::CRect::SetSize(long w, long h) {
